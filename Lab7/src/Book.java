8e2ad5afcda87677ead0ae3ae4b402ddc11e9a0c
4015d13 <4015d13@icaco.ece.uprm.edu>
2016-10-26 14:45:25 -0400

    Added Openable Interface for opening and closing objects

--- Lab7/src/Book.java
+++ Lab7/src/Book.java
@@ -1,5 +1,6 @@
 
-public class Book implements GameThing, Readable {
+public class Book implements GameThing, Readable, Openable {
+	 public boolean isOpen = false;      
 	  public String getName()
 
       {
@@ -12,7 +13,7 @@ public class Book implements GameThing, Readable {
 
       {
 
-            return "This book appears to be old and dusty. It's also quite heavy.";
+            return "This book appears to be old and its pages filled with dust. It's also quite heavy.";
 
       }
       public void read()
@@ -21,5 +22,26 @@ public class Book implements GameThing, Readable {
 
             System.out.println("It is written in some ancient language. You cannot decipher it yet.");
 
+      }
+      public void open() {
+    	  if( isOpen == false){
+    	  
+    	  System.out.print("As the book opens, you can feel a mysterious aura coming from it.");
+    	  isOpen = true;
+    	  }
+    	  else{
+    	  System.out.print("The book is already open!");
+    	  }
+      }
+      public void close() {
+    	  if( isOpen == true){
+        	  System.out.print("As the book closes, a cloud of dust is thrown into the area.");
+        	  isOpen = false;
+        	  }
+        	  else{
+        	  System.out.print("The book is already closed!");
+        	  }
+    	 
+    	  
       }
 }
